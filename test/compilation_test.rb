151d852e99cc31caba80a4837bcbd44f1b477aee
Ylan Segal <ylan@alxglobal.com>
2012-06-26 11:17:36 -0700

    Adding bootstrap-responsive-no-large-desktop

--- test/compilation_test.rb
+++ test/compilation_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CompilationTest < Test::Unit::TestCase
   def test_compilation
     path = 'vendor/assets/stylesheets'
-    %w(bootstrap bootstrap-responsive).each do |file|
+    %w(bootstrap bootstrap-responsive bootstrap-responsive-no-large-desktop).each do |file|
       engine = Sass::Engine.for_file("#{path}/_#{file}.scss", syntax: :scss, load_paths: [path])
       assert_nothing_raised do
         engine.render
